22024738413037c7dae13054290707f556a7ac59
HaDoCK <99925876+adrnl1ne@users.noreply.github.com>
2025-05-28 03:00:12 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,9 +1,19 @@
 <?php
+
 return [
+    # Database Configuration
+    'host'=>'sql206.infinityfree.com',
+    'dbname' =>'if0_39096893_chinook_autoincrement',
+    'user' =>'if0_39096893',
+    'password' => 'Z5QVZ6rJ9hZ7t',
+    'charset' => 'utf8mb4',
+]
+
+/*return [
     'host' => 'localhost:3306',
     'dbname' => 'chinook_autoincrement', // Your database name
     'user' => 'root', // Your MySQL username
     'password' => 'Nikolaj#14', // Your MySQL password (empty if none)
     'charset' => 'utf8mb4',
-];
+];*/
 ?>
